5eaef3a614a1cf1611509425bde3fd6cfaf2ae97
Mkortib <mkobria@gmail.com>
2020-04-04 02:04:20 +0300

    Init react app in the client folder using create-react-app, add ability run client side with server side app using proxy and concurrently package

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class App extends Component {
+  componentDidMount() {
+    axios.get('/api/product/brands').then((res) => console.log(res));
+  }
+
+  render() {
+    return <div className="">Hey</div>;
+  }
+}
+
+export default App;
