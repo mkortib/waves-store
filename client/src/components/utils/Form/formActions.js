10eeea6667a4a362049c1c25a5f9957cd7884e1b
Mkortib <mkobria@gmail.com>
2020-04-12 01:38:07 +0300

    Added register functionality. Installed new @babel dependency to solve  @material-ui issue

--- client/src/components/utils/Form/formActions.js
+++ client/src/components/utils/Form/formActions.js
@@ -8,6 +8,14 @@ export const validate = (element, formdata = []) => {
         error = !valid ? [valid, message] : error;
     }
 
+    if (element.validation.confirm) {
+        const valid =
+            element.value.trim() === formdata[element.validation.confirm].value;
+        const message = `${!valid ? 'Passwords do not match' : ''}`;
+
+        error = !valid ? [valid, message] : error;
+    }
+
     if (element.validation.required) {
         const valid = element.value.trim() !== '';
         const message = `${!valid ? 'This field is required' : ''}`;
@@ -46,7 +54,9 @@ export const generateData = (formdata, formName) => {
     let dataToSubmit = {};
 
     for (let key in formdata) {
-        dataToSubmit[key] = formdata[key].value;
+        if (key !== 'confirmPassword') {
+            dataToSubmit[key] = formdata[key].value;
+        }
     }
 
     return dataToSubmit;
--- client/src/hoc/layout.js
+++ client/src/hoc/layout.js
@@ -4,15 +4,15 @@ import Header from '../components/Header-footer/Header';
 import Footer from '../components/Header-footer/Footer';
 
 class Layout extends Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <div className="page_container">{this.props.children}</div>
-        <Footer />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Header />
+                <div className="page_container">{this.props.children}</div>
+                <Footer />
+            </div>
+        );
+    }
 }
 
 export default Layout;
