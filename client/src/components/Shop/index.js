84fd34edaf439744bedebbe88fcd702ec796d0fa
Mkortib <mkobria@gmail.com>
2020-04-19 01:19:03 +0300

    Made checkbox and radio components and related functionality

--- client/src/components/Shop/index.js
+++ client/src/components/Shop/index.js
@@ -3,14 +3,58 @@ import { connect } from 'react-redux';
 
 import PageTop from '../utils/page_top';
 import { getBrands, getWoods } from '../../actions/products_actions';
+import CollapseCheckbox from '../utils/collapseCheckbox';
+import CollapseRadio from '../utils/collapseRadio';
+
+import { frets, price } from '../utils/Form/fixed_categories';
 
 class Shop extends Component {
+    state = {
+        grid: '',
+        limit: 6,
+        skip: 0,
+        filters: {
+            brand: [],
+            frets: [],
+            wood: [],
+            price: [],
+        },
+    };
+
     componentDidMount() {
         this.props.dispatch(getBrands());
         this.props.dispatch(getWoods());
     }
 
+    handlePrice(value) {
+        const data = price;
+        let array = [];
+
+        for (let key in data) {
+            if (data[key]._id === parseInt(value, 10)) {
+                array = data[key].array;
+            }
+        }
+
+        return array;
+    }
+
+    handleFilters(filters, category) {
+        const newFilters = { ...this.state.filters };
+
+        newFilters[category] = filters;
+
+        if (category === 'price') {
+            let priceValues = this.handlePrice(filters);
+
+            newFilters[category] = priceValues;
+        }
+
+        this.setState({ filters: newFilters });
+    }
+
     render() {
+        console.log(this.state.filters);
         const products = this.props.products;
 
         return (
@@ -18,7 +62,40 @@ class Shop extends Component {
                 <PageTop title="Browse Products" />
                 <div className="container">
                     <div className="shop_wrapper">
-                        <div className="left">lleft</div>
+                        <div className="left">
+                            <CollapseCheckbox
+                                initState={true}
+                                title="Brands"
+                                list={products.brands}
+                                handleFilters={(filters) =>
+                                    this.handleFilters(filters, 'brand')
+                                }
+                            />
+                            <CollapseCheckbox
+                                initState={false}
+                                title="Frets"
+                                list={frets}
+                                handleFilters={(filters) =>
+                                    this.handleFilters(filters, 'frets')
+                                }
+                            />
+                            <CollapseCheckbox
+                                initState={true}
+                                title="Wood"
+                                list={products.woods}
+                                handleFilters={(filters) =>
+                                    this.handleFilters(filters, 'wood')
+                                }
+                            />
+                            <CollapseRadio
+                                initState={true}
+                                title="Proce"
+                                list={price}
+                                handleFilters={(filters) =>
+                                    this.handleFilters(filters, 'price')
+                                }
+                            />
+                        </div>
 
                         <div className="right">right</div>
                     </div>
