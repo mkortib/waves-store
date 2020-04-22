4c776cf5fc38cbfcc367d44b33365084c5e21fee
Mkortib <mkobria@gmail.com>
2020-04-23 01:45:25 +0300

    Made ProdNfo component.

--- client/src/components/Product/prodNfo.js
+++ client/src/components/Product/prodNfo.js
@@ -7,6 +7,74 @@ import faCheck from '@fortawesome/fontawesome-free-solid/faCheck';
 import faTimes from '@fortawesome/fontawesome-free-solid/faTimes';
 
 const ProdNfo = (props) => {
+    const showProdTags = (detail) => (
+        <div className="product_tags">
+            {/* !!!!!!!!!!!!!!! CHANGE SHIPPING */}
+            {!detail.shipping ? (
+                <div className="tag">
+                    <div className="">
+                        <FontAwesomeIcon icon={faTruck} />
+                    </div>
+                    <div className="tag_text">
+                        <div className="">free shipping</div>
+                        <div className="">And Return</div>
+                    </div>
+                </div>
+            ) : null}
+            {detail.available ? (
+                <div className="tag">
+                    <div className="">
+                        <FontAwesomeIcon icon={faCheck} />
+                    </div>
+                    <div className="tag_text">
+                        <div className="">Available</div>
+                        <div className="">in store</div>
+                    </div>
+                </div>
+            ) : (
+                <div className="tag">
+                    <div className="">
+                        <FontAwesomeIcon icon={faTimes} />
+                    </div>
+                    <div className="tag_text">
+                        <div className="">Not Available</div>
+                        <div className="">Preorder only</div>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+
+    const showProdActions = (detail) => (
+        <div className="product_actions">
+            <div className="price">$ {detail.price}</div>
+            <div className="cart">
+                <MyButton
+                    type="add_to_cart_link"
+                    runAction={() => {
+                        console.log('Add to cart');
+                    }}
+                />
+            </div>
+        </div>
+    );
+
+    const showProdSpecifications = (details) => (
+        <div className="product_specifications">
+            <h2>Specifications</h2>
+            <div className="">
+                <div className="item">
+                    <strong>Frets:</strong>
+                    {details.frets}
+                </div>
+                <div className="item">
+                    <strong>Wood:</strong>
+                    {details.wood.name}
+                </div>
+            </div>
+        </div>
+    );
+
     const { details } = props;
     return (
         <div className="">
@@ -14,6 +82,9 @@ const ProdNfo = (props) => {
                 {details.brand.name} {details.name}
             </h1>
             <p>{details.description}</p>
+            {showProdTags(details)}
+            {showProdActions(details)}
+            {showProdSpecifications(details)}
         </div>
     );
 };
